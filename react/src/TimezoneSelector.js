895eb6d0a8363de5062d80d244e913dba442c857
minjong-ha <hmjong1128@gmail.com>
2023-04-27 17:56:10 +0900

    timezone: Seperate timezones and cities with togglable submenues

--- react/src/TimezoneSelector.js
+++ react/src/TimezoneSelector.js
@@ -2,11 +2,12 @@ import React, {useState} from 'react';
 import PropTypes from 'prop-types';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faChevronDown, faChevronUp } from '@fortawesome/free-solid-svg-icons';
-import { timeZoneNames } from './timeZoneNames';
+import { timeZoneNames, timeZoneCities } from './timeZoneNames';
 import './TimezoneSelector.css';
 
 const TimezoneSelector = ({ onChange }) => {
   const [isOpen, setIsOpen] = useState(false);
+  const [expandedCities, setExpandedCities] = useState({});
 
   const handleTimezoneClick = (timezone) => {
     onChange(timezone);
@@ -16,6 +17,16 @@ const TimezoneSelector = ({ onChange }) => {
     setIsOpen(!isOpen);
   };
 
+  const toggleCitiesList = (timezone) => {
+    setExpandedCities((prevState) => ({
+          ...prevState,
+          [timezone]: !prevState[timezone],
+          }));
+  };
+
+
+  console.log(timeZoneCities);
+
   return (
       <div className="timezone-selector">
       <h4 onClick={toggleTimezoneList}>
@@ -24,17 +35,39 @@ const TimezoneSelector = ({ onChange }) => {
       </h4>
       <ul className={isOpen ? 'open' : ''}>
       {timeZoneNames.map((timezone) => (
-            <li
-            key={timezone}
+
+            <li key={timezone}>
+            <div
             className="timezone-option"
-            onClick={() => handleTimezoneClick(timezone)}
+            onClick={() => toggleCitiesList(timezone)}
             >
-            {timezone}
-            </li>
-            ))}
+            {timezone}{' '}
+            <FontAwesomeIcon
+            icon={expandedCities[timezone] ? faChevronUp : faChevronDown}
+            />
+            </div>
+            <ul
+            className={`timezone-cities ${
+            expandedCities[timezone] ? 'open' : ''
+            }`}
+            >
+            {timeZoneCities[timezone].map((city) => (
+                  <li
+                  key={city}
+                  className="timezone-city"
+                  onClick={() => handleTimezoneClick(city)}
+                  >
+                  {city}
+                  </li>
+                  ))}
       </ul>
-      </div>
-      );
+        </li>
+
+
+        ))}
+  </ul>
+    </div>
+    );
 };
 
 TimezoneSelector.propTypes = {
--- react/src/timeZoneNames.js
+++ react/src/timeZoneNames.js
@@ -1,6 +1,28 @@
 import moment from 'moment-timezone';
-export const timeZoneNames = moment.tz.names();
 
+const zones = moment.tz.names();
+const timeZoneNames = [];
+const timeZoneCities = {};
+
+zones.forEach((zoneName) => {
+    const zone = moment.tz.zone(zoneName);
+    const offset = zone.utcOffset(moment());
+
+    // Ignore timezones with non-integer offsets
+    if (offset % 60 !== 0) return;
+
+    const hours = offset / 60;
+    const utcLabel = hours >= 0 ? `UTC+${hours}` : `UTC${hours}`;
+
+    if (!timeZoneNames.includes(utcLabel)) {
+    timeZoneNames.push(utcLabel);
+    timeZoneCities[utcLabel] = [];
+    }
+
+    timeZoneCities[utcLabel].push(zoneName);
+    });
+
+export { timeZoneNames, timeZoneCities };
 //export const timeZoneNames = [
 //  'UTC',
 //  'America/New_York',
